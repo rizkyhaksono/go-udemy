f9564a7a16c8d2381f7630696928ca2b3b5cb3e5
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-04-08 00:21:33 +0700

    update: new model to call function

--- deck.go
+++ deck.go
@@ -1,4 +1,28 @@
 package main
 
+import "fmt"
+
 // create a new type of deck, which is a slice of string
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string {"Spades", "Diamonds", "Hearts", "Clubs"}
+
+	cardValues := []string {"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, val := range cardValues {
+			cards = append(cards, suit + " of " + val)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) printDeck() {
+	for index, card := range d {
+		fmt.Println(index, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	// old way
-	// var card string = "Joker"
+	cards := newDeck()
 
-	// new way
-	card := deck{
-		newCard(),
-		newCard(),
-		"Joker",
-		"Ace of Diamonds",
-	}
-
-	card = append(card, "Six of Spades")
-
-	// card = "Ace of Spades"
-
-	// fmt.Println(card)
-
-	// foreach
-	for index, card := range card {
-		fmt.Println(index, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.printDeck()
 }
