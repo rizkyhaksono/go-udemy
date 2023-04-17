f8b2014dd31fb44f134436e23633ad366c3a5601
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-04-17 21:23:26 +0700

    add: testing on deck

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -16,7 +17,7 @@ func newDeck() deck {
 	cardValues := []string {"Ace", "Two", "Three", "Four"}
 	for _, suit := range cardSuits {
 		for _, val := range cardValues {
-			cards = append(cards, suit + " of " + val)
+			cards = append(cards, val + " of " + suit)
 		}
 	}
 	return cards
@@ -52,8 +53,11 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
