26623801917d8ab13f6887375e5c25f0159d7a30
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-05-16 08:54:46 +0700

    add: response body

--- http/main.go
+++ http/main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://infotech.umm.ac.id/")
-
+	resp, err := http.Get("http://www.google.com/")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	bs := make([] byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+
+	// fmt.Println(resp)
 }
