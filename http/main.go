43f587359f87c5e1183b11df5c330a0b8240a9b7
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-05-18 16:23:31 +0700

    update: write function

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://www.google.com/")
@@ -13,9 +16,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([] byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	// bs := make([] byte, 99999)
+	// resp.Body.Read(bs)
 
-	// fmt.Println(resp)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+	// fmt.Println(io.Copy(os.Stdout, resp.Body))
+
+	// fmt.Println(string(bs))
+}
+
+func (logWriter) Write (bs[] byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+
+	return len(bs), nil
 }
